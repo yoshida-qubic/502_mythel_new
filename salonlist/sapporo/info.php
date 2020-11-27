3ed39fc6143aae1b46230b2bcd39ff51f7a8e45a
miura <miura@qu-bic.jp>
2020-11-27 17:20:08 +0900

    札幌大通店：電話番号追加

--- salonlist/sapporo/info.php
+++ salonlist/sapporo/info.php
@@ -10,11 +10,11 @@ $access = '札幌市営地下鉄 大通駅から徒歩1分';
 $room_number = '10';
 $machine = '脱毛2台・キャビテーション3台・フェイシャル2台・ホワイトニング2台・ネイル1台';
 
-$tel = '未定';
+$tel = '011-219-2231';
 
 $map = '<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5830.316514127228!2d141.351826!3d43.059139!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDPCsDAzJzMyLjkiTiAxNDHCsDIxJzA2LjYiRQ!5e0!3m2!1sja!2sus!4v1605578521141!5m2!1sja!2sus" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>';
 
-$tel_tag = '';
+$tel_tag = '0112192231';
 
 $map_link = 'https://goo.gl/maps/RSmXj4MfKAvmskcz6';
 
