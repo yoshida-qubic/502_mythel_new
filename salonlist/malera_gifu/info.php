6ce532f347590bf07342299886098322795f3818
miura <miura@qu-bic.jp>
2020-12-14 18:32:19 +0900

    モレラ岐阜店：電話番号追加

--- salonlist/malera_gifu/info.php
+++ salonlist/malera_gifu/info.php
@@ -9,11 +9,11 @@ $access = '樽見鉄道「モレラ岐阜駅」より徒歩1分';
 $room_number = '';
 $machine = '脱毛2台・キャビテーション4台・フェイシャル2台・ホワイトニング2台・ネイル1台・マグウェーブ1台';
 
-$tel = '未定';
+$tel = '058-372-8660';
 
 $map = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3249.993988766165!2d136.6704233222444!3d35.4549432497706!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6003ad5829555555%3A0x6bc8aa89577883de!2z44Oi44Os44Op5bKQ6Zic!5e0!3m2!1sja!2sjp!4v1605576670294!5m2!1sja!2sjp" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>';
 
-$tel_tag = '';
+$tel_tag = '0583728660';
 
 $map_link = 'https://goo.gl/maps/6mbNJNEQeZjooNfcA';
 
