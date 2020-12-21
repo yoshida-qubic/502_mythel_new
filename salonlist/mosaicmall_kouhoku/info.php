ebb7210a2faaaa372185f3c1f5193aebcbf25e4f
miura <miura@qu-bic.jp>
2020-12-21 13:20:59 +0900

    モザイクモール港北店：電話番号追加

--- salonlist/mosaicmall_kouhoku/info.php
+++ salonlist/mosaicmall_kouhoku/info.php
@@ -9,11 +9,11 @@ $access = '横浜市営地下鉄（ブルーライン・グリーンライン）
 $room_number = '';
 $machine = '脱毛2台・キャビテーション4台・フェイシャル2台・ホワイトニング2台・ネイル1台・マグウェーブ1台';
 
-$tel = '未定';
+$tel = '045-914-2209';
 
 $map = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3245.94394580081!2d139.57567265117865!3d35.55508654438951!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6018f637df924feb%3A0x8253089ee6d069be!2z44Oi44K244Kk44Kv44Oi44O844Or5riv5YyX!5e0!3m2!1sja!2sjp!4v1605577179049!5m2!1sja!2sjp" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>';
 
-$tel_tag = '';
+$tel_tag = '0459142209';
 
 $map_link = 'https://goo.gl/maps/JxnKhwBDEud7hChx7';
 
