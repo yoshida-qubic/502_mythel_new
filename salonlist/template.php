a5f04b74a2ce9f5412e8e78a03f92aff3711fd37
a-nishikawa <a-nishikawa@qu-bic.jp>
2021-04-02 10:33:24 +0900

    サロンリスト一覧、札幌大通店、元に戻す

---
+++ salonlist/template.php
@@ -0,0 +1,24 @@
+<div class="item">
+  <div class="inner">
+    <h4><?php echo $salonname; ?></h4>
+    <dl>
+      <dt>定休日：</dt>
+      <dd><?php echo $holiday; ?></dd>
+    </dl>
+    <dl>
+      <dt>営業時間：</dt>
+      <dd><?php echo $buisiness_hours; ?></dd>
+    </dl>
+    <dl>
+      <dt>所在地：</dt>
+      <dd><?php echo $address; ?></dd>
+    </dl>
+    <dl>
+      <dt>TEL：</dt>
+      <dd><?php echo $tel; ?></dd>
+    </dl>
+  </div>
+
+  <div class="link purple"><a href="<?php echo $slug; ?>/">店舗の詳細はこちら ▶︎</a></div>
+
+</div>
