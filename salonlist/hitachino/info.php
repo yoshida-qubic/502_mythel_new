14dd6a89ff89adf34480474c24013d771e2ac6ef
a-nishikawa <a-nishikawa@qu-bic.jp>
2022-02-01 12:09:19 +0900

    ひたちの店：電話番号追加

--- salonlist/hitachino/info.php
+++ salonlist/hitachino/info.php
@@ -10,7 +10,7 @@ $access = 'JRひたち野「うしく駅」より徒歩8分';
 $room_number = '';
 $machine = '脱毛器 1台・キャビテーション 2台・フェイシャル 1台・ホワイトニング 1台・MAGWAVE EX 1台・ハイフ 1台';
 
-$tel = '未定';
+$tel = '029-869-6111';
 
 $map = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3227.625272345921!2d140.1600520156914!3d36.00502168012005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6022723e0438bb5d%3A0x8adccd00fe0426da!2z44CSMzAwLTEyMDcg6Iyo5Z-O55yM54mb5LmF5biC44Gy44Gf44Gh6YeO5p2x77yV5LiB55uu77yY4oiS77yU!5e0!3m2!1sja!2sjp!4v1643682483397!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>';
 
