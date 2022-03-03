7ee96d7ce8fc47bc1229a3fb4fbf1f4b8c3131a0
a-nishikawa <a-nishikawa@qu-bic.jp>
2022-03-03 12:39:36 +0900

    mozoワンダーシティ店：電話番号変更

--- salonlist/mozo_wondercity/info.php
+++ salonlist/mozo_wondercity/info.php
@@ -10,7 +10,7 @@ $access = '名鉄・地下鉄「上小田井駅」北口より徒歩約5分、�
 $room_number = '13';
 $machine = '脱毛器 2台・ラジオ波 3台・キャビテーション 1台・フェイシャル 1台・ホワイトニング 1台・MAGWAVE EX 3台・ハイフ 1台・コラーゲンマシン 1台';
 
-$tel = '052-228-9975';
+$tel = '052-908-8584';
 
 $map = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1629.6092913632422!2d136.88206480827947!3d35.225928395075684!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x600374682edcce19%3A0x951319ee874bbccc!2zbW96b-ODr-ODs-ODgOODvOOCt-ODhuOCow!5e0!3m2!1sja!2sjp!4v1643679380941!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>';
 
