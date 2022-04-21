82d09de71f9e09f8870f9f636f07537e5e17d720
a-nishikawa <a-nishikawa@qu-bic.jp>
2022-04-21 15:21:17 +0900

    MEGAドン・キホーテ豊橋店、アルザ泉大津店追加

---
+++ salonlist/alzar_izumiotsu/info.php
@@ -0,0 +1,31 @@
+<?php
+$area = '関西エリア';
+$salonname = 'アルザ泉大津店';
+$salontype = '';
+$holiday = '不定休<span>（研修等で店休になる場合があります）</span>';
+$buisiness_hours = 'OPEN 10:00 〜 CLOSE 20:00';
+$address = '〒595-0025 大阪府泉大津市旭町20番地1号 アルザタウン泉大津 2F';
+$access = '南海本線「泉大津駅」より徒歩1分';
+
+$room_number = '';
+$machine = '脱毛1台・キャビテーション1台・ラジオ波1台・フェイシャル1台・MAGWAVE EX2台・コラーゲンマシン1台';
+
+$tel = '未定';
+
+$map = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3288.025884435872!2d135.40497671559064!3d34.50222805132886!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6000c453f7d4003b%3A0x9c3f9420acc77fb4!2z44Ki44Or44K244K_44Km44Oz5rOJ5aSn5rSl!5e0!3m2!1sja!2sjp!4v1650521384250!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+
+$tel_tag = str_replace(array('-', 'ー', '−', '―', '‐'), '', $tel);
+
+$map_link = 'https://goo.gl/maps/yQS5sJUhSM8DAXxZ9';
+
+
+
+
+$reserved = '';
+
+$slug = 'alzar_izumiotsu';
+
+$schedule = '';
+
+
+?>
---
+++ salonlist/megadonki_toyohashi/info.php
@@ -0,0 +1,31 @@
+<?php
+$area = '東海エリア';
+$salonname = 'MEGAドン・キホーテ豊橋店';
+$salontype = '';
+$holiday = '不定休<span>（研修等で店休になる場合があります）</span>';
+$buisiness_hours = 'OPEN 10:00 〜 CLOSE 21:00';
+$address = '〒441-8061 愛知県豊橋市藤沢町141 MEGAドン・キホーテ豊橋 シネマ棟2F';
+$access = '豊橋鉄道渥美線「愛知大学前駅」より徒歩約20分、「豊橋駅」東口より豊鉄バス 小浜大崎線「藤沢町」下車徒歩1分';
+
+$room_number = '';
+$machine = '脱毛1台・キャビテーション1台・ラジオ波1台・フェイシャル1台・ハイフ1台・MAGWAVE EX1台';
+
+$tel = '0532-39-5623';
+
+$map = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3278.475679257603!2d137.36962021559478!3d34.743608438426!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6004d2205ce188a7%3A0x326bc2c1dfd67153!2zTUVHQeODieODs-ODu-OCreODm-ODvOODhiDosYrmqYvlupc!5e0!3m2!1sja!2sjp!4v1650520584933!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+
+$tel_tag = str_replace(array('-', 'ー', '−', '―', '‐'), '', $tel);
+
+$map_link = 'https://goo.gl/maps/CHNiwmSUD7ZRrADVA';
+
+
+
+
+$reserved = '';
+
+$slug = 'megadonki_toyohashi';
+
+$schedule = '';
+
+
+?>
