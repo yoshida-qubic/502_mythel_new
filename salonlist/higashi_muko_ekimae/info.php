9a88d5756c040d397db6929f2e814ced15205783
yamanaka-qubic <m.yamanaka@qu-bic.jp>
2022-12-27 20:02:46 +0900

    阪急東向日駅前店、鹿島店　追加

---
+++ salonlist/higashi_muko_ekimae/info.php
@@ -0,0 +1,30 @@
+<?php
+$area = '関西エリア';
+$salonname = '阪急東向日駅前店';
+$salontype = '';
+$holiday = '水曜日';
+$buisiness_hours = '平日 OPEN 10:00 〜 CLOSE 20:00 / 土日祝 OPEN 10:00 〜 CLOSE 18:00';
+$address = '〒617-0002 京都府向日市寺戸町小佃11-3 1F';
+$access = '阪急京都本線「東向日駅」より徒歩4分、JR東海道・山陽本線「向日町駅」より徒歩10分';
+
+$room_number = '';
+$machine = '脱毛1台・キャビテーション1台・フェイシャル1台・ラジオ派1台・MAGWAVE EX1台・ハーブテントスチームアップ1台・シルクライト1台';
+
+$tel = '070-9007-9723';
+
+$map = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1635.0609297588962!2d135.70190555837863!3d34.95355429509257!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6001042797f8714b%3A0xfc89acb7c7fd51f2!2z44CSNjE3LTAwMDIg5Lqs6YO95bqc5ZCR5pel5biC5a-65oi455S65bCP5L2D77yR77yR!5e0!3m2!1sja!2sjp!4v1672106583776!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+
+$tel_tag = str_replace(array('-', 'ー', '−', '―', '‐'), '', $tel);
+
+$map_link = 'https://goo.gl/maps/4mU1vatyM7W53EDm8';
+
+
+
+
+$reserved = '';
+
+$slug = 'higashi_muko_ekimae';
+
+$schedule = '2月2日';
+
+$instagram_link = '';
---
+++ salonlist/kashima/info.php
@@ -0,0 +1,30 @@
+<?php
+$area = '関東エリア';
+$salonname = '鹿島店';
+$salontype = '';
+$holiday = '水曜日';
+$buisiness_hours = '平日、土 OPEN 10:00 〜 CLOSE 20:00 / 日祝 OPEN 10:00 〜 CLOSE 18:00';
+$address = '〒314-0031 茨城県鹿嶋市宮中4670-4 2F';
+$access = 'JR鹿島線「鹿島神宮駅」から車で5分、JR鹿島線「鹿島サッカースタジアム駅」から徒歩21分';
+
+$room_number = '';
+$machine = '脱毛1台・キャビテーション1台・フェイシャル1台・ホワイトニング1台・ラジオ派1台';
+
+$tel = '050-1467-7050';
+
+$map = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3228.6270493939305!2d140.63674171621554!3d35.980542480125706!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60225509f5eb8a45%3A0xda8995803b7d709f!2z44CSMzE0LTAwMzEg6Iyo5Z-O55yM6bm_5baL5biC5a6u5Lit77yU77yW77yX77yQ4oiS77yU!5e0!3m2!1sja!2sjp!4v1672108051247!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+
+$tel_tag = str_replace(array('-', 'ー', '−', '―', '‐'), '', $tel);
+
+$map_link = 'https://goo.gl/maps/wU8DruYWrV3PLkNs9';
+
+
+
+
+$reserved = '';
+
+$slug = 'kashima';
+
+$schedule = '';
+
+$instagram_link = '';
