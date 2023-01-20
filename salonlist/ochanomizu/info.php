f3349ca0ef70db2399f4f8680416f61a07a262b5
yamanaka-qubic <m.yamanaka@qu-bic.jp>
2023-01-20 19:44:21 +0900

    御茶ノ水店　追加

---
+++ salonlist/ochanomizu/info.php
@@ -0,0 +1,30 @@
+<?php
+$area = '関東エリア';
+$salonname = '御茶ノ水店';
+$salontype = '';
+$holiday = '不定休<span>（研修等で店休になる場合があります）</span>';
+$buisiness_hours = '未定';
+$address = '〒101-0062 東京都千代田区神田駿河台2-1-20 お茶の水ユニオンビルB1F';
+$access = 'JR中央線「御茶ノ水駅」より徒歩2分、東京メトロ千代田線「新御茶ノ水駅」より徒歩4分、東京メトロ半蔵門線「神保町駅」より徒歩11分';
+
+$room_number = '';
+$machine = '脱毛1台・ラジオ波1台・フェイシャル1台・MAGWAVE EX2台・ハーブテントスチームアップ1台・シルクライト1台';
+
+$tel = '未定';
+
+$map = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3240.077297969603!2d139.76214!3d35.699715399999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188c1764f3e7c9%3A0x255fe37e72362def!2z44CSMTAxLTAwNjIg5p2x5Lqs6YO95Y2D5Luj55Sw5Yy656We55Sw6ae_5rKz5Y-w77yS5LiB55uu77yR4oiS77yS77yQIOOBiuiMtuOBruawtOODpuODi-OCquODs-ODk-ODqw!5e0!3m2!1sja!2sjp!4v1674197640333!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+
+$tel_tag = str_replace(array('-', 'ー', '−', '―', '‐'), '', $tel);
+
+$map_link = 'https://goo.gl/maps/XLyRz2sVHLbgSBes7';
+
+
+
+
+$reserved = '';
+
+$slug = 'ochanomizu';
+
+$schedule = '';
+
+$instagram_link = '';
